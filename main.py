05e9637c9bd1134a0176a69f1be2e8b1c68a812d
Stephen Adegbokun <obaadegbokun1@gmail.com>
2023-05-25 14:34:59 +0100

    remove unsorted table

--- main.py
+++ main.py
@@ -30,16 +30,9 @@ def timetable():
 
     courses["start_time"] = courses["start_time"].apply(split_data_and_return_time)
     courses["finish_time"] = courses["finish_time"].apply(split_data_and_return_time)
-    
-    unsorted_time_table = PrettyTable(field_names=courses.columns.tolist(), title="Unsorted Table")
-    for _,row in courses.iterrows():
-        unsorted_time_table.add_row(row.values)
     sorted_courses = courses.sort_values(by=['finish_time'], ignore_index=True)
 
-    print(unsorted_time_table)
-
     venue_result_list = []
-
     for i,row in sorted_courses.iterrows():
         if i == 0:
             for venue in list_of_venues:
